0f7792e312b0007d085dfa0e9223d7560b83ea09
Joc4daPadoc4 <quim.doiche@gmail.com>
2024-11-09 00:16:03 -0300

    Add files via upload

---
+++ scirpt.js
@@ -0,0 +1,8 @@
+function closeOverlay() {
+    document.getElementById('donationOverlay').style.display = 'none';
+  }
+  
+  function openOverlay() {
+    document.getElementById('donationOverlay').style.display = 'flex';
+  }
+  
